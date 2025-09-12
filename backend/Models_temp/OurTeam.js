c74a20756000e677d39d8a5be25e67a9ecb28b8c
Shivanshu <devpathtechnology>
2025-09-12 15:19:59 +0530

    Remove temporary Models_temp folder

--- backend/Models_temp/OurTeam.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ourTeamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  position: { type: String, required: true },
-  image: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("OurTeam", ourTeamSchema);
